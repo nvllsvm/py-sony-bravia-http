74d65b657caa9a20bbac12991293b5257d2d2d51
Andrew Rabert <ar@nullsum.net>
2025-06-10 14:11:03 -0400

    Reformat with ruff

--- src/lounge_tv_http/__main__.py
+++ src/lounge_tv_http/__main__.py
@@ -7,19 +7,18 @@ import lounge_tv_http.app
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--host', default='0.0.0.0')
-    parser.add_argument('--port', type=int, default=8000)
-    parser.add_argument('--lounge-tv-cmd',
-                        default=lounge_tv_http.app._DEFAULT_COMMAND)
-    parser.add_argument('--serial-device',
-                        default=lounge_tv_http.app._DEFAULT_DEVICE)
+    parser.add_argument("--host", default="0.0.0.0")
+    parser.add_argument("--port", type=int, default=8000)
+    parser.add_argument("--lounge-tv-cmd", default=lounge_tv_http.app._DEFAULT_COMMAND)
+    parser.add_argument("--serial-device", default=lounge_tv_http.app._DEFAULT_DEVICE)
     args = parser.parse_args()
 
     uvicorn.run(
         lounge_tv_http.app.create_app(args.lounge_tv_cmd, args.serial_device),
         host=args.host,
-        port=args.port)
+        port=args.port,
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- src/lounge_tv_http/app.py
+++ src/lounge_tv_http/app.py
@@ -19,7 +19,7 @@ logger = logging_config.configure()()
 
 
 class LoungeTV:
-    INPUT_TYPE_HDMI = 'hdmi'
+    INPUT_TYPE_HDMI = "hdmi"
 
     def __init__(self, command, serial_device):
         self.command = command
@@ -28,168 +28,167 @@ class LoungeTV:
     async def _run(self, *args):
         proc = await asyncio.create_subprocess_exec(
             self.command,
-            '--dev', self.serial_device,
+            "--dev",
+            self.serial_device,
             *args,
             stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE)
+            stderr=asyncio.subprocess.PIPE,
+        )
         stdout, stderr = await proc.communicate()
         if proc.returncode != 0:
             raise RuntimeError(
-                f'returncode {proc.returncode} '
-                f'(args: {args}) '
-                f'(stderr: {stderr})')
+                f"returncode {proc.returncode} (args: {args}) (stderr: {stderr})"
+            )
         return stdout
 
     async def on(self):
-        await self._run('power', 'on')
+        await self._run("power", "on")
 
     async def off(self):
-        await self._run('power', 'off')
+        await self._run("power", "off")
 
     async def power(self):
-        await self._run('power', 'toggle')
+        await self._run("power", "toggle")
 
     async def display(self):
-        await self._run('display', 'toggle')
+        await self._run("display", "toggle")
 
     async def picture_on(self):
-        await self._run('picture', 'on')
+        await self._run("picture", "on")
 
     async def picture_off(self):
-        await self._run('picture', 'off')
+        await self._run("picture", "off")
 
     async def picture(self):
-        await self._run('picture', 'toggle')
+        await self._run("picture", "toggle")
 
     async def volume_up(self):
-        await self._run('volume', 'up')
+        await self._run("volume", "up")
 
     async def volume_down(self):
-        await self._run('volume', 'down')
+        await self._run("volume", "down")
 
     async def mute(self):
-        await self._run('mute', 'toggle')
+        await self._run("mute", "toggle")
 
     async def brightness_up(self):
-        await self._run('brightness', 'up')
+        await self._run("brightness", "up")
 
     async def brightness_down(self):
-        await self._run('brightness', 'down')
+        await self._run("brightness", "down")
 
     async def brightness_min(self):
-        await self._run('brightness', 'min')
+        await self._run("brightness", "min")
 
     async def brightness_max(self):
-        await self._run('brightness', 'max')
+        await self._run("brightness", "max")
 
     async def input_select(self, input_type, input_num):
-        await self._run(f'input-{input_type}', f'{input_num}')
+        await self._run(f"input-{input_type}", f"{input_num}")
 
     async def status(self):
-        return await self._run('status')
+        return await self._run("status")
 
 
 class BrightnessController(Controller):
-    path = '/brightness'
-    tags = ['brightness']
+    path = "/brightness"
+    tags = ["brightness"]
 
-    @get('/up')
+    @get("/up")
     async def up(self, state: State) -> None:
         await state.lounge_tv.brightness_up()
 
-    @get('/down')
+    @get("/down")
     async def down(self, state: State) -> None:
         await state.lounge_tv.brightness_down()
 
-    @get('/min')
+    @get("/min")
     async def min(self, state: State) -> None:
         await state.lounge_tv.brightness_min()
 
-    @get('/max')
+    @get("/max")
     async def max(self, state: State) -> None:
         await state.lounge_tv.brightness_max()
 
 
 class DisplayController(Controller):
-    path = '/display'
-    tags = ['display']
+    path = "/display"
+    tags = ["display"]
 
-    @get('/toggle')
+    @get("/toggle")
     async def toggle(self, state: State) -> None:
         await state.lounge_tv.display()
 
 
 class PictureController(Controller):
-    path = '/picture'
-    tags = ['picture']
+    path = "/picture"
+    tags = ["picture"]
 
-    @get('/on')
+    @get("/on")
     async def on(self, state: State) -> None:
         await state.lounge_tv.picture_on()
 
-    @get('/off')
+    @get("/off")
     async def off(self, state: State) -> None:
         await state.lounge_tv.picture_off()
 
-    @get('/toggle')
+    @get("/toggle")
     async def toggle(self, state: State) -> None:
         await state.lounge_tv.picture()
 
 
 class PowerController(Controller):
-    path = '/power'
-    tags = ['power']
+    path = "/power"
+    tags = ["power"]
 
-    @get('/on')
+    @get("/on")
     async def on(self, state: State) -> None:
         await state.lounge_tv.on()
 
-    @get('/off')
+    @get("/off")
     async def off(self, state: State) -> None:
         await state.lounge_tv.off()
 
-    @get('/toggle')
+    @get("/toggle")
     async def toggle(self, state: State) -> None:
         await state.lounge_tv.power()
 
 
 class VolumeController(Controller):
-    path = '/volume'
-    tags = ['volume']
+    path = "/volume"
+    tags = ["volume"]
 
-    @get('/up')
+    @get("/up")
     async def up(self, state: State) -> None:
         await state.lounge_tv.volume_up()
 
-    @get('/down')
+    @get("/down")
     async def down(self, state: State) -> None:
         await state.lounge_tv.volume_down()
 
-    @get('/mute/toggle')
+    @get("/mute/toggle")
     async def mute_toggle(self, state: State) -> None:
         await state.lounge_tv.mute()
 
 
 class InputSelectController(Controller):
-    path = '/input'
-    tags = ['input']
+    path = "/input"
+    tags = ["input"]
 
-    @get('/hdmi/{port:int}')
+    @get("/hdmi/{port:int}")
     async def hdmi_1(
         self,
         state: State,
         port: Annotated[int, Parameter(ge=1, le=5)],
     ) -> None:
-        await state.lounge_tv.input_select(
-            state.lounge_tv.INPUT_TYPE_HDMI, port)
+        await state.lounge_tv.input_select(state.lounge_tv.INPUT_TYPE_HDMI, port)
 
 
-_DEFAULT_COMMAND = 'sony-bravia-cli'
-_DEFAULT_DEVICE = '/dev/ttyUSB0'
+_DEFAULT_COMMAND = "sony-bravia-cli"
+_DEFAULT_DEVICE = "/dev/ttyUSB0"
 
 
-def create_app(lounge_tv_cmd=_DEFAULT_COMMAND,
-               serial_device=_DEFAULT_DEVICE):
+def create_app(lounge_tv_cmd=_DEFAULT_COMMAND, serial_device=_DEFAULT_DEVICE):
     return Litestar(
         logging_config=logging_config,
         route_handlers=[
@@ -200,5 +199,5 @@ def create_app(lounge_tv_cmd=_DEFAULT_COMMAND,
             PowerController,
             VolumeController,
         ],
-        state=State({'lounge_tv': LoungeTV(lounge_tv_cmd, serial_device)}),
+        state=State({"lounge_tv": LoungeTV(lounge_tv_cmd, serial_device)}),
     )
